e98d20c6d2f7ac523017e49833dfe8c195066d23
PhatTN <phattnse90070@gmail.com>
2015-10-28 07:14:44 +0700

    Change from using Apache HttpClient UriBuilder to using native Uri.Builder of Android SDK. Adds more helper methods.

--- app/src/androidTest/java/com/example/phat/vnexpressnews/util/URLUtilsTest.java
+++ app/src/androidTest/java/com/example/phat/vnexpressnews/util/URLUtilsTest.java
@@ -7,7 +7,12 @@ import org.junit.Test;
 import org.junit.runner.RunWith;
 
 import java.net.URISyntaxException;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.HashMap;
 import java.util.LinkedHashMap;
+import java.util.List;
 import java.util.Map;
 
 import static org.hamcrest.CoreMatchers.equalTo;
@@ -17,7 +22,7 @@ import static org.junit.Assert.assertFalse;
 import static org.junit.Assert.assertTrue;
 
 /**
- * Tests for {@link URLUtils}. Because this class depend on some
+ * Tests for {@link URLUtils}. Because {@link URLUtils} class depends on some
  * lib in Android SDK. So, it should be tested by instrumented unit test
  */
 @RunWith(AndroidJUnit4.class)
@@ -26,18 +31,27 @@ public class URLUtilsTest {
 
     private static final String TEST_WEB_URL_CORRECT_FORMAT = "https://example.com/demo";
     private static final String TEST_WEB_URL_CORRECT_FORMAT_WITH_ARGUMENT = "http://www.example.com.vn/demo?arg1=hello&arg2=%20world";
+    private static final String TEST_WEB_URL_CORRECT_FORMAT_WITH_SQUARE_BRACKETS = "https://www.example/demo?api%5B%5D?arg1=value1";
     private static final String TEST_WEB_URL_INCORRECT_FORMAT_NO_DOMAIN_NAME = "HTtp://example/demo";
     private static final String TEST_WEB_URL_INCORRECT_FORMAT = "https://.example.com/demo";
     private static final String TEST_WEB_URL_INCORRECT_SCHEME = "file://example.com";
     private static final String TEST_WEB_URL_NULL = null;
     private static final String TEST_WEB_URL_EMPTY = "";
     private static final Map<String, String> MOCK_REQUEST_PARAMETER_MAP;
+    private static final Map<String, List<String>> MOCK_REQUEST_PARAMETERS_FOR_COMPLEX_URL;
 
     static {
-        MOCK_REQUEST_PARAMETER_MAP = new LinkedHashMap<String, String>() {{ // Use LinkedHashMap to remain the order
+        MOCK_REQUEST_PARAMETER_MAP = new LinkedHashMap<String, String>(3) {{ // Use LinkedHashMap to remain the order
             put("params1", "Param value 1"); // try put space in value
             put("params2", "243"); // try put value as number
             put("params 3", "ParamValue3"); // try put space in key
+        }};
+
+        MOCK_REQUEST_PARAMETERS_FOR_COMPLEX_URL = new LinkedHashMap<String, List<String>>(4) {{
+            put("name1", Arrays.asList("Param value 1", "234", "Param1")); // Tests with three values, and tests encoding value
+            put("name2", Collections.singletonList("Param value 2")); // Tests with only one value
+            put("name 3", null);                                            // Tests with value is null
+            put("name 4", new ArrayList<String>());                         // Tests with value is empty
         }};
     }
 
@@ -51,6 +65,12 @@ public class URLUtilsTest {
         assertTrue(URLUtils.isValidURL(TEST_WEB_URL_CORRECT_FORMAT_WITH_ARGUMENT));
     }
 
+    @Test
+    public void checkValidURL_CorrectURLFormatWithSquareBrackets_ReturnsTrue() {
+        assertTrue(URLUtils.isValidURL(TEST_WEB_URL_CORRECT_FORMAT_WITH_SQUARE_BRACKETS));
+    }
+
+    @Test
     public void checkValidURL_InCorrectURLFormatNoDomainName_ReturnsFalse() {
         assertFalse(URLUtils.isValidURL(TEST_WEB_URL_INCORRECT_FORMAT_NO_DOMAIN_NAME));
     }
@@ -75,16 +95,168 @@ public class URLUtilsTest {
         assertFalse(URLUtils.isValidURL(TEST_WEB_URL_EMPTY));
     }
 
+    /**
+     *  Tests buildURL(String,Map<String,String>) method
+     */
     @Test(expected = URISyntaxException.class)
-    public void checkBuildURL_InCorrectURLFormat_ThrowsURISE() throws URISyntaxException {
+    public void checkBuildURL_InCorrectURLFormat_ThrowsUSE() throws URISyntaxException {
         URLUtils.buildURL(TEST_WEB_URL_INCORRECT_FORMAT, MOCK_REQUEST_PARAMETER_MAP);
     }
 
-    @Test
-    public void checkBuildURL_CorrectURLFormat_ReturnsTrue() throws URISyntaxException{
-        final String expectedURL = "https://example.com/demo?params1=Param+value+1&params2=243&params+3=ParamValue3";
-        assertThat(URLUtils.buildURL(TEST_WEB_URL_CORRECT_FORMAT, MOCK_REQUEST_PARAMETER_MAP),
-                is(equalTo(expectedURL)));
+    /**
+     *  Tests buildURL(String,Map<String,String>) method
+     */
+    @Test(expected = NullPointerException.class)
+    public void checkBuildURL_NullURL_ThrowsNPE() throws URISyntaxException {
+        URLUtils.buildURL(null, MOCK_REQUEST_PARAMETER_MAP);
     }
 
+    /**
+     *  Tests buildURL(String,Map<String,String>) method
+     */
+    @Test
+    public void checkBuildURL_CorrectURLFormat_ReturnsTrue() throws URISyntaxException{
+        final String expectedURL = "https://example.com/demo?params1=Param%20value%201&params2=243&params%203=ParamValue3";
+        final String builtURL = URLUtils.buildURL(TEST_WEB_URL_CORRECT_FORMAT, MOCK_REQUEST_PARAMETER_MAP);
+        assertTrue(URLUtils.isValidURL(builtURL));
+        assertThat(builtURL, is(equalTo(expectedURL)));
+    }
+
+    /**
+     *  Tests buildURL(String,Map<String,String>) method
+     */
+    @Test
+    public void checkBuildURL_NullRequestParameters_ReturnsTrue() throws URISyntaxException{
+        final String expectedURL = "https://example.com/demo";
+        final Map<String, String> nullRequestParameters = null;
+        final String builtURL = URLUtils.buildURL(TEST_WEB_URL_CORRECT_FORMAT, nullRequestParameters);
+        assertThat(builtURL, is(equalTo(expectedURL)));
+    }
+
+    @Test(expected = IllegalStateException.class)
+    public void checkBuildSubURL_NullPath_ThrowsISE() throws URISyntaxException {
+        URLUtils.buildSubURL(null, MOCK_REQUEST_PARAMETER_MAP);
+    }
+
+    @Test(expected = IllegalStateException.class)
+    public void checkBuildSubURL_NullRequestParameters_ThrowsISE() throws URISyntaxException {
+        final Map<String, String> nullRequestParameters = null;
+        URLUtils.buildSubURL("api/demo", nullRequestParameters);
+    }
+
+    @Test
+    public void checkBuildSubURL_EmptyRequestParameters_ReturnsTrue() throws URISyntaxException {
+        final String expectedResult = "api/demo";
+        final Map<String, String> emptyRequestParameters = new HashMap<>();
+        final String builtResult = URLUtils.buildSubURL("api/demo", emptyRequestParameters);
+        assertThat(builtResult, is(equalTo(expectedResult)));
+    }
+
+    @Test
+    public void checkBuildSubURL_CorrectFormat_ReturnsTrue() throws URISyntaxException {
+        final String expectedResult = "api/demo?params1=Param%20value%201&params2=243&params%203=ParamValue3";
+        final String builtResult = URLUtils.buildSubURL("api/demo", MOCK_REQUEST_PARAMETER_MAP);
+        assertThat(builtResult, is(equalTo(expectedResult)));
+    }
+
+    /**
+     *  Tests buildURL(String,String) method
+     */
+    @Test
+    public void checkBuildURL_CorrectFormatURL_ReturnsTrue() throws URISyntaxException {
+        final String path = "api/demo";
+        final String correctFormatURL = "https://example.com";
+        final String expectedResult = "https://example.com/api/demo";
+        final String builtResult = URLUtils.buildURL(correctFormatURL, path);
+        assertThat(builtResult, is(equalTo(expectedResult)));
+    }
+
+    /**
+     *  Tests buildURL(String,String) method
+     */
+    @Test
+    public void checkBuildURL_CorrectFormatURLCase2_ReturnsTrue() throws URISyntaxException {
+        final String path = "api/demo";
+        final String correctFormatURL = "https://example.com/path";
+        final String expectedResult = "https://example.com/api/demo";
+        final String builtResult = URLUtils.buildURL(correctFormatURL, path);
+        assertThat(builtResult, is(equalTo(expectedResult)));
+    }
+
+    /**
+     *  Tests buildURL(String,String) method
+     */
+    @Test(expected = URISyntaxException.class)
+    public void checkBuildURL_IncorrectFormatURL_ThrowsUSE() throws URISyntaxException {
+        final String path = "api/demo";
+        final String incorrectFormatURL = "https://example";
+        URLUtils.buildURL(incorrectFormatURL, path);
+    }
+
+    /**
+     *  Tests buildURL(String,String) method
+     */
+    @Test(expected = NullPointerException.class)
+    public void checkBuildURL_NullURLString_ThrowsNPE() throws URISyntaxException {
+        final String path = "api/demo";
+        final String nullURL = null;
+        URLUtils.buildURL(nullURL, path);
+    }
+
+    /**
+     *  Tests buildURL(String,String) method
+     */
+    @Test(expected = URISyntaxException.class)
+    public void checkBuildURL_EmptyURL_ThrowsUSE() throws URISyntaxException {
+        final String path = "api/demo";
+        final String emptyURL = "";
+        URLUtils.buildURL(emptyURL, path);
+    }
+
+    /**
+     *  Tests buildURL(String,String) method
+     */
+    @Test
+    public void checkBuildURL_NullPath_ReturnsTrue() throws URISyntaxException {
+        final String path = null;
+        final String correctFormatURL = "https://example.com";
+        final String expectedResult = "https://example.com";
+        final String builtResult = URLUtils.buildURL(correctFormatURL, path);
+        assertThat(builtResult, is(equalTo(expectedResult)));
+    }
+
+    /**
+     *  Tests buildURL(String,String) method
+     */
+    @Test
+    public void checkBuildURL_EmptyPath_ReturnsTrue() throws URISyntaxException {
+        final String path = "";
+        final String correctFormatURL = "https://example.com";
+        final String expectedResult = "https://example.com";
+        final String builtResult = URLUtils.buildURL(correctFormatURL, path);
+        assertThat(builtResult, is(equalTo(expectedResult)));
+    }
+
+    @Test(expected = NullPointerException.class)
+    public void checkBuildComplexURL_NullURL_ThrowsNPE() throws URISyntaxException {
+        URLUtils.buildComplexURL(null, MOCK_REQUEST_PARAMETERS_FOR_COMPLEX_URL);
+    }
+
+    @Test(expected = URISyntaxException.class)
+    public void checkBuildComplexURL_EmptyURL_ThrowsUSE() throws URISyntaxException {
+        URLUtils.buildComplexURL("", MOCK_REQUEST_PARAMETERS_FOR_COMPLEX_URL);
+    }
+
+    @Test(expected = URISyntaxException.class)
+    public void checkBuildComplexURL_IncorrectFormatURL_ThrowsUSE() throws URISyntaxException {
+        URLUtils.buildComplexURL(TEST_WEB_URL_INCORRECT_FORMAT_NO_DOMAIN_NAME, MOCK_REQUEST_PARAMETERS_FOR_COMPLEX_URL);
+    }
+
+    @Test
+    public void checkBuildComplexURL_CorrectFormatURL_EqualsExpectedResult() throws URISyntaxException {
+        final String expectedResult = "https://example.com/demo?name1=Param%20value%201&name1=234&name1=Param1&name2=Param%20value%202";
+        final String actualResult = URLUtils.buildComplexURL(TEST_WEB_URL_CORRECT_FORMAT,
+                MOCK_REQUEST_PARAMETERS_FOR_COMPLEX_URL);
+        assertThat(actualResult, is(equalTo(expectedResult)));
+    }
 }
--- app/src/main/java/com/example/phat/vnexpressnews/util/URLUtils.java
+++ app/src/main/java/com/example/phat/vnexpressnews/util/URLUtils.java
@@ -1,15 +1,13 @@
 package com.example.phat.vnexpressnews.util;
 
-import android.text.TextUtils;
+import android.net.Uri;
+import android.support.annotation.NonNull;
 import android.util.Patterns;
 
-import org.apache.http.client.utils.URIBuilder;
-
 import java.net.URISyntaxException;
-import java.util.Iterator;
+import java.util.List;
 import java.util.Map;
 
-import static com.example.phat.vnexpressnews.util.LogUtils.LOGE;
 import static com.example.phat.vnexpressnews.util.LogUtils.LOGI;
 import static com.example.phat.vnexpressnews.util.LogUtils.makeLogTag;
 
@@ -46,31 +44,136 @@ public class URLUtils {
 
         // Check url argument is valid or not
         if (!isValidURL(url)) {
-            LOGE(TAG, "Passed URL String is invalid. URL: " +url);
+            if (url == null) {
+                throw new NullPointerException("Passed URL Argument must not be null");
+            }
             throw new URISyntaxException(url, "Passed URL Argument is invalid");
         }
 
-        try {
-            URIBuilder uriBuilder = new URIBuilder(url);
+        Uri.Builder uriBuilder = Uri.parse(url).buildUpon();
 
+        if (requestParameters != null) {
             // Loop through requestParameter Map and add parameter to urlBuilder
-            Iterator<String> keys = requestParameters.keySet().iterator();
-            while (keys.hasNext()) {
-                String key = keys.next();
+            for (String key : requestParameters.keySet()) {
                 String value = requestParameters.get(key);
-
-                uriBuilder.addParameter(key, value);
+                uriBuilder.appendQueryParameter(key, value);
             }
-
-            String builtUrl = uriBuilder.build().toString();
-
-            LOGI(TAG, "Build url successful. Url is: " +builtUrl);
-            return builtUrl;
-
-        } catch (URISyntaxException e) {
-            LOGE(TAG, "Passed URL is unhealthy. Url: " +url);
-            throw new URISyntaxException(e.getInput(), e.getReason(), e.getIndex());
         }
+
+        String builtUrl = uriBuilder.build().toString();
+
+        LOGI(TAG, "Build url successful. Url is: " +builtUrl);
+        return builtUrl;
     }
 
+    /**
+     * SubURL term means incomplete URL. It does not include scheme, host. It only includes
+     * path and query string. Some API Endpoint in this app are weird. It composes multi sub url
+     * in query string as:
+     * http://api3.vnexpress.net/api/group?api[]=article?type=get_topstory&limit=15&api[]=article?type=get_rule_2
+     * In above API Endpoint has 2 sub url: article?type=get_topstory&limit=15 and article?type=get_rule_2
+     * Their format like a query string, but in fact, both of them are "value" role for "api[]" request name.
+     * So we created this method to build sub url like that for some API Endpoint.
+     *
+     */
+    public static String buildSubURL(@NonNull String path, Map<String, String> requestParameters)
+            throws URISyntaxException {
+
+        if (TextUtils.isEmpty(path)) {
+            throw new IllegalStateException("The first parameter must not be null or empty.");
+        }
+
+        if (requestParameters == null) {
+            throw new IllegalStateException("The request parameters must not be null.");
+        }
+
+        Uri.Builder uriBuilder = new Uri.Builder();
+        uriBuilder.path(path);
+
+        // Loop through requestParameter Map and add parameter to urlBuilder
+        for (String key : requestParameters.keySet()) {
+            String value = requestParameters.get(key);
+            uriBuilder.appendQueryParameter(key, value);
+        }
+
+        String builtUrl = uriBuilder.build().toString();
+
+        LOGI(TAG, "Build sub url successful. Sub Url is: " +builtUrl);
+        return builtUrl;
+    }
+
+    /**
+     * Build a url with its path. This method will remove existed path of URL,
+     * then set its path to {@code path} param
+     *
+     * @param url  The URL which you want to build upon
+     * @param path The path which is appended to the URL
+     * @throws URISyntaxException if either the URL or the path is invalid
+     */
+    public static String buildURL(String url, String path) throws URISyntaxException {
+
+        if (!isValidURL(url)) {
+            if (url == null) {
+                throw new NullPointerException("Passed URL Argument must not be null");
+            }
+            throw new URISyntaxException(url, "Passed URL Argument is invalid");
+        }
+
+        Uri.Builder uriBuilder = Uri.parse(url).buildUpon();
+
+        if (!TextUtils.isEmpty(path)) {
+            uriBuilder.path(path);
+        }
+
+        String builtUrl = uriBuilder.build().toString();
+        LOGI(TAG, "Build url successful. Url is: " +builtUrl);
+        return builtUrl;
+    }
+
+    /**
+     * Basically, this method has same function as {@code buildURL<String, Map<String, String>} method.
+     * But, in some cases, an URL includes complex query string. The query string has one "name" contains
+     * multi values. Example: https://example.com/api?param=TheFirst&param=TheSecond&param=TheThird&app_id=111.
+     * In the above example URL, the name "param" repeats three times and includes different value.
+     * So for building that URL is this methods purpose.
+     *
+     * @param url  The URL which you want to build upon
+     * @param requestParameters  A request parameter map
+     */
+    public static String buildComplexURL(String url, Map<String, List<String>> requestParameters)
+            throws URISyntaxException {
+        // Check url argument is valid or not
+        if (!isValidURL(url)) {
+            if (url == null) {
+                throw new NullPointerException("Passed URL Argument must not be null");
+            }
+            throw new URISyntaxException(url, "Passed URL Argument is invalid");
+        }
+
+        Uri.Builder uriBuilder = Uri.parse(url).buildUpon();
+
+        if (requestParameters != null) {
+            // Loop through requestParameter Map and add parameter to urlBuilder
+            for (String key : requestParameters.keySet()) {
+                List<String> values = requestParameters.get(key);
+                if (values == null || values.isEmpty()) {
+                    continue;
+                }
+
+                if (values.size() == 1) {
+                    uriBuilder.appendQueryParameter(key, values.get(0));
+                    continue;
+                }
+
+                for (String v : values) {
+                    uriBuilder.appendQueryParameter(key, v);
+                }
+            }
+        }
+
+        String builtUrl = uriBuilder.build().toString();
+
+        LOGI(TAG, "Build url successful. Url is: " +builtUrl);
+        return builtUrl;
+    }
 }
